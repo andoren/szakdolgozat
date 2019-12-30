a697bda1408ae3266e1fa5bd76c685d26ef292b5
andoren <mpekar55@gmail.com>
2019-12-30 14:50:08 +0100

    gRPCServer projekt init és DLL hozzáadása refként + gRPC server elinditása még csak LOGIN

---
+++ C#/IktatogRPCServer/IktatogRPCServer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Grpc.Core;
+using Iktato;
+namespace IktatogRPCServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartServer();
+        }
+        Server server;
+        const int Port = 1991;
+        public void StartServer()
+        {
+            try
+            {
+
+                server = new Server
+                {
+                    Services = { IktatoService.BindService(new Service.SerivceForgRPC ()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure)}
+                };
+                server.Start();
+                MessageBox.Show("The server has started successfully");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ C#/IktatogRPCServer/IktatogRPCServer/Service/SerivceForgRPC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Iktato;
+namespace IktatogRPCServer.Service
+{
+    class SerivceForgRPC:IktatoService.IktatoServiceBase
+    {
+        public override Task<User> Login(LoginMessage request, ServerCallContext context)
+        {
+            return Task.FromResult<User>(new User() { Id = 1, Username ="Misi", Fullname= "Pekár Mihály", Privilege = new Privilege() { Id = 1, Name = "Admin"} }) ;
+        }
+        public override Task<Answer> Logout(User request, ServerCallContext context)
+        {
+            return base.Logout(request, context);
+        }
+        public override Task<Answer> Register(User request, ServerCallContext context)
+        {
+          
+            return base.Register(request, context);
+        }
+    }
+}
