1b567a4d04110774169ee621d40a133ff2adf585
andoren <mpekar55@gmail.com>
2019-12-30 17:01:40 +0100

    Caliburn.Micro élesítése és mappák létre hozása  cliensben illetve login View és ViewModel init

---
+++ C#/IktatogRPCClient/IktatogRPCClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using Grpc.Core;
+using Iktato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IktatogRPCClient.ViewModels
+{
+    class LoginViewModel:Screen
+    {
+        public LoginViewModel()
+        {
+
+        }
+        async Task connectToServer()
+        {
+            string csatinfo = "localhost" + ":" + "1991";
+            Channel channel = new Channel(csatinfo, ChannelCredentials.Insecure);
+            IktatoService.IktatoServiceClient client = new IktatoService.IktatoServiceClient(channel);
+            LoginMessage login = new LoginMessage() { Username = "misi", Password = "Kiscica" };
+            User user = await client.LoginAsync(login);
+            MessageBox.Show(user.Fullname);
+
+        }
+        public async void LoginButton() {
+            await connectToServer();
+        }
+    }
+}
