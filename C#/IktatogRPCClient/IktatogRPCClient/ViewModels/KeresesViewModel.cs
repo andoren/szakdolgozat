e3ca5ff2c39b8298e116cb0924875ab480b3e178
andoren <mpekar55@gmail.com>
2020-02-16 15:57:31 +0100

    Kereses design view terv elkészítve

--- C#/IktatogRPCClient/IktatogRPCClient/ViewModels/KeresesViewModel.cs
+++ C#/IktatogRPCClient/IktatogRPCClient/ViewModels/KeresesViewModel.cs
@@ -9,5 +9,54 @@ namespace IktatogRPCClient.ViewModels
 {
     class KeresesViewModel:Screen
     {
-    }
+		public KeresesViewModel()
+		{
+			SearchList = new BindableCollection<string>() { "Iktatószám","Tárgy","Partner", "Jelleg", "Ügyintéző", "Csoport" };
+			Iranyok = new BindableCollection<string>() { "Bejövő", "Kimenő","Összes" };
+			SelectedSearchParameter = SearchList[0];
+			SelectedIranyParameter = Iranyok[0];
+		}
+		private string _selectedSearchParameter;
+
+		public string SelectedSearchParameter
+		{
+			get { return _selectedSearchParameter; }
+			set {
+				_selectedSearchParameter = value;
+				NotifyOfPropertyChange(() => SelectedSearchParameter);
+			}
+		}
+		private string _selectedIranyParameter;
+
+		public string SelectedIranyParameter
+		{
+			get { return _selectedIranyParameter; }
+			set { 
+				_selectedIranyParameter = value;
+				NotifyOfPropertyChange(() => SelectedIranyParameter);
+			}
+		}
+
+		private BindableCollection<string> _searchList;
+
+		public BindableCollection<string> SearchList
+		{
+			get { return _searchList; }
+			set {
+				_searchList = value;
+				NotifyOfPropertyChange(() => SearchList);
+			}
+		}
+		private BindableCollection<string> _iranyok;
+
+		public BindableCollection<string> Iranyok
+		{
+			get { return _iranyok; }
+			set {
+				_iranyok = value;
+				NotifyOfPropertyChange(() => Iranyok);
+			}
+		}
+
+	}
 }
