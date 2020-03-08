31441c1dc0ed261fc061a5d173bb5496c452f898
andoren <mpekar55@gmail.com>
2020-03-08 13:41:46 +0100

    Szerver gui Inditás és Leállítás gomb üzembe helyezése

--- C#/IktatogRPCServer/IktatogRPCServer/MainWindow.xaml.cs
+++ C#/IktatogRPCServer/IktatogRPCServer/MainWindow.xaml.cs
@@ -14,7 +14,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Grpc.Core;
+using System.Threading;
+using System.Management;
+
 using Iktato;
+using System.Diagnostics;
+using System.Windows.Threading;
+using Microsoft.VisualBasic.Devices;
+
 namespace IktatogRPCServer
 {
     /// <summary>
@@ -25,8 +32,21 @@ namespace IktatogRPCServer
         public MainWindow()
         {
             InitializeComponent();
-            StartServer();
+            //StartServer();
+            mySelf = Process.GetCurrentProcess();
+            theCPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            theMemCounter = new PerformanceCounter("Process", "Private Bytes",mySelf.ProcessName);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(250);
+            timer.Tick += SetProcessLabel;
+            timer.Start();
+          
+
         }
+        Process mySelf;
+        PerformanceCounter theCPUCounter;
+        PerformanceCounter theMemCounter;
+        int AllMemoryInMb = Convert.ToInt32((new ComputerInfo().TotalPhysicalMemory / (Math.Pow(1024, 2))) + 0.5);
         Server server;
         const int Port = 443;
         public void StartServer()
@@ -45,13 +65,49 @@ namespace IktatogRPCServer
                     
                 };
                 server.Start();
-               
+                StartServerButton.IsEnabled = false;
+                StopServerButton.IsEnabled = true;
 
-        }
+            }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-}
+        }
+        private void SetProcessLabel(object sender, EventArgs e) {
+
+            
+          
+            float processor = theCPUCounter.NextValue() / Environment.ProcessorCount;
+            float memory = theMemCounter.NextValue()/(1024*1024);
+            ProcessorLabel.Text = string.Format("{0}%",(int)processor);
+            MemoryLable.Text = $"{(int)memory}MB/{AllMemoryInMb}MB";
+            CpuBar.Value = (int)processor;
+            MemoryBar.Value = (int)((((memory) / AllMemoryInMb) * 100));
+
+        }
+
+        private void StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            StartServer();
+        }
+
+        private void StopServerAndQuit_Click(object sender, RoutedEventArgs e)
+        {
+            if (server != null) {
+                server.ShutdownAsync();
+            }
+            this.Close();
+        }
+
+        private void StopServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (server != null)
+            {
+                server.ShutdownAsync();
+                StartServerButton.IsEnabled = true;
+                StopServerButton.IsEnabled = false;
+            }
+        }
     }
 }
