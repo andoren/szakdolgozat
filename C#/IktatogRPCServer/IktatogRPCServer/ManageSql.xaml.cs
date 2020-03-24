8054a9a406a978cc6063b4efd4e6c6518d49537f
andoren <mpekar55@gmail.com>
2020-03-24 10:36:43 +0100

    adatbázis visszatöltése működik

--- C#/IktatogRPCServer/IktatogRPCServer/ManageSql.xaml.cs
+++ C#/IktatogRPCServer/IktatogRPCServer/ManageSql.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using IktatogRPCServer.Database;
+using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using MySql.Data.MySqlClient;
 using Serilog;
@@ -13,6 +14,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -34,8 +36,12 @@ namespace IktatogRPCServer
         }
         private bool IsErrorOccured = false;
         private ConnectionManager connectionManager;
-        private void SaveSqlButton_Click(object sender, RoutedEventArgs e)
+
+
+
+        private async void SaveSqlButton_Click(object sender, RoutedEventArgs e)
         {
+            SaveProgressBar.IsIndeterminate = true;
             string constring = connectionManager.ConnectionString;
             string file = $"{Directory.GetCurrentDirectory()}\\Backup\\backup.sql";
             var dlg = new CommonOpenFileDialog();
@@ -53,85 +59,123 @@ namespace IktatogRPCServer
             dlg.ShowPlacesList = true;
             if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                try
-                {
-                    file = dlg.FileName +$"\\{DateTime.Now.ToString().Replace(".","").Replace(":","")}IktatoSQLBackup.sql";
 
-                    using (MySqlConnection conn = new MySqlConnection(constring))
+                await Task.Run(() =>
+                {
+                    try
                     {
-                        using (MySqlCommand cmd = new MySqlCommand())
+
+                        file = dlg.FileName + $"\\{DateTime.Now.ToString().Replace(".", "").Replace(":", "")}IktatoSQLBackup.sql";
+                        
+                        using (MySqlConnection conn = new MySqlConnection(constring))
                         {
-                            using (MySqlBackup mb = new MySqlBackup(cmd))
+                            using (MySqlCommand cmd = new MySqlCommand())
                             {
-                                cmd.Connection = conn;
-                                conn.Open();
-                                mb.ExportInfo.AddCreateDatabase = true;
-                                mb.ExportInfo.ExportTableStructure = true;
-                                mb.ExportInfo.ExportRows = true;
-                                mb.ExportInfo.ExportRoutinesWithoutDefiner = true;
-                                mb.ExportInfo.ExportProcedures = true;
-                                mb.ExportInfo.ExportFunctions = true;
-                                mb.ExportInfo.ExportViews = true;
-                                mb.ExportInfo.AddDropDatabase = true;
-                                mb.ExportInfo.AddDropTable = true;
-                                mb.ExportInfo.IntervalForProgressReport = 50;
-                                mb.ExportInfo.GetTotalRowsMode = GetTotalRowsMethod.SelectCount;
-                                mb.ExportProgressChanged += Mb_ExportProgressChanged;
-                                SaveProgressBar.Value = 0;
-                                SavePrecentage.Text = $"0%";
-                                SaveStatus.Text = $"";
-                                mb.ExportCompleted += Mb_ExportCompleted;
-                                mb.ExportToFile(file);
-                       
-                                conn.Close();
-            
+                                using (MySqlBackup mb = new MySqlBackup(cmd))
+                                {
+                                    cmd.Connection = conn;
+                                    conn.Open();
+                                    mb.ExportInfo.AddCreateDatabase = true;
+                                    mb.ExportInfo.ExportTableStructure = true;
+                                    mb.ExportInfo.ExportRows = true;
+                                    mb.ExportInfo.ExportRoutinesWithoutDefiner = true;
+                                    mb.ExportInfo.ExportProcedures = true;
+                                    mb.ExportInfo.ExportFunctions = true;
+                                    mb.ExportInfo.ExportViews = true;
+                                    mb.ExportInfo.AddDropDatabase = true;
+                                    mb.ExportInfo.AddDropTable = true;
+                                    mb.ExportInfo.IntervalForProgressReport = 50;
+                                    mb.ExportInfo.GetTotalRowsMode = GetTotalRowsMethod.SelectCount;
+                                    mb.ExportToFile(file);
+
+
+                                    conn.Close();
+
+                                }
                             }
                         }
+                        Dispatcher.Invoke(()=>{
+                            PathToSave.Text = file;
+                            SaveProgressBar.IsIndeterminate = false;
+                            SaveStatus.Text = $"Az adatbázis mentése sikeresen befejeződött.";
+                            if (!IsErrorOccured) MessageBox.Show("Sikeres mentés.");
+                            else
+                            {
+                                SaveStatus.Text = $"Hiba az adatbázis mentése közben.";
+
+                            }
+                        });
+
+
                     }
-                    PathToSave.Text = file;
-                    SaveProgressBar.Value = 100;
-                    SavePrecentage.Text = $"100%";
-                    SaveStatus.Text = $"Az adatbázis mentése sikeresen befejeződött.";
-                    if (!IsErrorOccured) MessageBox.Show("Sikeres mentés.");
-                    else {
-                        SaveStatus.Text = $"Hiba az adatbázis mentése közben.";
-                       
+                    catch (Exception ex)
+                    {
+                        Log.Error("Hiba történt az adatbázis mentése során. {Message}", ex.Message);
+                        MessageBox.Show("Hiba történt.");
                     }
-                }
-                catch (Exception ex) {
-                    Log.Error("Hiba történt az adatbázis mentése során. {Message}",ex.Message);
-                    MessageBox.Show("Hiba történt.");
-                }
+                });
             }
 
-    
+
         }
 
-        private void Mb_ExportCompleted(object sender, ExportCompleteArgs e)
+
+        private async void ImportSqlButton_Click(object sender, RoutedEventArgs e)
         {
-            IsErrorOccured = e.HasError;
+            ImportProgressBar.IsIndeterminate = true;
+            OpenFileDialog dialog = new OpenFileDialog();
+            string file;
+            string constring = connectionManager.ConnectionString;
+            dialog.Filter = "SQL script| *sql;";
+            dialog.Multiselect = false;
+            dialog.CheckFileExists = true;
+            dialog.Title = "Script kiválasztása";
+            if ((bool)dialog.ShowDialog())
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+
+                        IsErrorOccured = false;
+                        file = dialog.FileName;
+                       
+                        using (MySqlConnection conn = new MySqlConnection(constring))
+                        {
+                            using (MySqlCommand cmd = new MySqlCommand())
+                            {
+                                using (MySqlBackup mb = new MySqlBackup(cmd))
+                                {
+                                    cmd.Connection = conn;
+                                    conn.Open();
+                                    
+                                    mb.ImportFromFile(file);
+                                    conn.Close();
+
+                                }
+                            }
+                        }
+                        Dispatcher.Invoke(()=> {
+                            ImportPath.Text = file;
+                            ImportProgressBar.IsIndeterminate = false;
+                            ImportStatus.Text = $"Az adatbázis importálása sikeresen befejeződött.";
+                            if (!IsErrorOccured) MessageBox.Show("Sikeres importálás.");
+                            else
+                            {
+                                SaveStatus.Text = $"Hiba az adatbázis importálása közben.";
+
+                            }
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Hiba történt az adatbázis importálása során. {Message}", ex.Message);
+                        MessageBox.Show("Hiba történt.");
+                    }
+                });
+            }
         }
 
-        private long AllRowCount = 0;
-        private long SavedRowCount = 0;
-        private void Mb_ExportProgressChanged(object sender, ExportProgressArgs e)
-        {
-            Dispatcher.Invoke(()=> {
-                if (SaveProgressBar.Value !=100) { 
-                AllRowCount = e.TotalRowsInAllTables;
-                SavedRowCount += e.TotalRowsInCurrentTable;
-                int percentage = (int)(((double)SavedRowCount / AllRowCount) * 100);
-                SaveProgressBar.Value = percentage;
-                SavePrecentage.Text = $"{percentage}%";
-                SaveStatus.Text = $"{e.CurrentTableName} tábla mentése.";
-                }
 
-            });
-        }
-
-        private void ImportSqlButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
