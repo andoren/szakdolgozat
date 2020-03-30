a5e3dcbfc18ebd4b53b93ef2068f62858bd0bc53
andoren <mpekar55@gmail.com>
2020-03-30 11:35:05 +0200

    SHA1 tiktokistás implementálása a kliensben

---
+++ C#/IktatogRPCClient/IktatogRPCClient/Models/Managers/Helpers/Client/EncryptionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IktatogRPCClient.Models.Managers.Helpers.Client
+{
+    class EncryptionHelper
+    {
+        public static string EncryptSha1(string password)
+        {
+            
+            byte[] temp2;
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            temp2 = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < temp2.Length; i++)
+            {
+                sb.Append(temp2[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
