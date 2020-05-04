883e36c510aeec304a459b60bcd143a0164dc973
andoren <mpekar55@gmail.com>
2020-05-04 09:42:42 +0200

    interfészek, és service-k létrehozva

---
+++ C#/IktatogRPCServer/IktatogRPCServer/Database/Interfaces/IDatabaseConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿using Iktato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IktatogRPCServer.Database.Interfaces
+{
+    public interface IDatabaseConnectionManager
+    {
+        ConnectionManager ConnectionManager { get; set; }
+        void OpenConnection(object connection);
+        void CloseConnection(object connection);
+    }
+}
---
+++ C#/IktatogRPCServer/IktatogRPCServer/Database/Services/IkonyvService.cs
@@ -0,0 +1,13 @@
+﻿using IktatogRPCServer.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IktatogRPCServer.Database.Services
+{
+    class IkonyvService:IManageIkonyv
+    {
+    }
+}
